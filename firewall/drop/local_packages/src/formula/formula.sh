3f720fbaa19905e015ff375c79ce71dbc7c96ba4
Everton Segur <everton.segur@zup.com.br>
2020-08-18 23:21:31 -0300

    feat(default net applied) configured

--- firewall/drop/local_packages/src/formula/formula.sh
+++ firewall/drop/local_packages/src/formula/formula.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
-runFormula() {
+
+
+
+function apply_rules() {
   sudo iptables -I INPUT -s $NETWORK_ADDRESS -j DROP;
   sudo iptables -I INPUT -s $GATEWAY_ADDRESS -j ACCEPT;
+}
+
+function apply_without_params() {
+  
+  HOSTNAME=$(hostname -I | cut -d ' ' -f1);
+  PARSED_ADDRESS=$(echo $HOSTNAME | sed 's/\./io/g');
+  FIRST=$(echo $PARSED_ADDRESS | cut -d 'i' -f1 );
+  SECOND=$(echo $PARSED_ADDRESS | cut -d 'i' -f2 | cut -d 'o' -f2 );
+  THIRD=$(echo $PARSED_ADDRESS | cut -d 'i' -f3 | cut -d 'o' -f2);
+
+  NETWORK_ADDRESS=$(echo "$(echo $FIRST).$(echo $SECOND).$(echo $THIRD)".0/24);
+
+  sudo iptables -I INPUT -s $NETWORK_ADDRESS -j DROP;
+  sudo iptables -I INPUT -s $HOSTNAME -j ACCEPT;
+}
+
+runFormula() {
+  if [[ $GATEWAY_ADDRESS != "null" ]] 
+  then
+    apply_rules;
+  else
+    apply_without_params;
+  fi;
 }
